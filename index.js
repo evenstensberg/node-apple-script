6113e33bd95346c2070eb8b3b5cba3063fe7fd45
Even Stensberg <evenstensberg@gmail.com>
2016-09-08 13:37:31 +0200

    update API
    Thanks to Sindre Sorhos

--- index.js
+++ index.js
@@ -1,99 +1,30 @@
-exports.lightsUp = function(){
-  const spawn = require('child_process').spawn;
-  const ls = spawn('osascript', ['-e', 'tell application "System Events" to key code 113']);
-}
+const {execFileSync} = require('child_process');
 
-exports.lightsDown = function() {
-  const spawn_2 = require('child_process').spawn;
-  const ls = spawn_2('osascript', ['-e', 'tell application "System Events" to key code 107']);
-}
-exports.highlightPrevious = function() {
-  const spawn_3 = require('child_process').spawn;
-  const ls = spawn_3('osascript', ['-e', 'tell application "System Events" to key code 126 using command down']);
-}
-exports.densityDown = function() {
-  const spawn_4 = require('child_process').spawn;
-  const ls = spawn_4('osascript', ['-e', 'tell application "System Events" to key code 96']);
-}
-exports.densityUp = function() {
-  const spawn_5 = require('child_process').spawn;
-  const ls = spawn_5('osascript', ['-e', 'tell application "System Events" to key code 97']);
+const create = script => () => execFileSync('osascript', ['-e', script]);
+const createKey = key => create(`tell application "System Events" to key code ${key}`);
+const createKeyCmd = key => createKey(`${key} using command down`);
 
-}
-exports.launchPad = function() {
-  const spawn_6 = require('child_process').spawn;
-  const ls = spawn_6('osascript', ['-e', 'tell application "System Events" to key code 131']);
-}
-
-exports.splitScreen = function() {
-  const spawn_7 = require('child_process').spawn;
-  const ls = spawn_7('osascript', ['-e', 'tell application "System Events" to key code 160']);
-}
-exports.spreadScreen = function() {
-  const spawn_8 = require('child_process').spawn;
-  const ls = spawn_8('osascript', ['-e', 'tell application "System Events" to key code 103']);
-}
-exports.closeWindow = function() {
-  const spawn_9 = require('child_process').spawn;
-  const ls = spawn_9('osascript', ['-e', 'tell application "System Events" to key code 13 using command down']);
-}
-exports.closeEverything = function() {
-  const spawn_10 = require('child_process').spawn;
-  const ls = spawn_10('osascript', ['-e', 'tell application "System Events" to key code 12 using command down']);
-}
-exports.newTab = function() {
-  const spawn_10 = require('child_process').spawn;
-  const ls = spawn_10('osascript', ['-e', 'tell application "System Events" to key code 17 using command down']);
-}
-exports.newWindow = function() {
-  const spawn_11 = require('child_process').spawn;
-  const ls = spawn_11('osascript', ['-e', 'tell application "System Events" to key code 45 using command down']);
-}
-exports.splitTabs = function() {
-  const spawn_12 = require('child_process').spawn;
-  const ls = spawn_12('osascript', ['-e', 'tell application "System Events" to key code 2 using command down']);
-}
-exports.search = function() {
-  const spawn_12 = require('child_process').spawn;
-  const ls = spawn_12('osascript', ['-e', 'tell application "System Events" to key code 3 using command down']);
-}
-exports.clear = function() {
-  const spawn_13 = require('child_process').spawn;
-  const ls = spawn_13('osascript', ['-e', 'tell application "System Events" to key code 40 using command down']);
-}
-exports.paste = function() {
-  const spawn_14 = require('child_process').spawn;
-  const ls = spawn_14('osascript', ['-e', 'tell application "System Events" to key code 9 using command down']);
-}
-exports.hide = function() {
-  const spawn_15 = require('child_process').spawn;
-  const ls = spawn_15('osascript', ['-e', 'tell application "System Events" to key code 46 using command down']);
-}
-exports.zoom = function() {
-  const spawn_16 = require('child_process').spawn;
-  const ls = spawn_16('osascript', ['-e', 'tell application "System Events" to key code 27 using command down']);
-}
-exports.config = function() {
-  const spawn_17 = require('child_process').spawn;
-  const ls = spawn_17('osascript', ['-e', 'tell application "System Events" to key code 43 using command down']);
-}
-exports.minimize = function() {
-  const spawn_18 = require('child_process').spawn;
-  const ls = spawn_18('osascript', ['-e', 'tell application "System Events" to key code 29 using command down']);
-}
-exports.shutDown = function() {
-  const spawn_19 = require('child_process').spawn;
-  const ls = spawn_19('osascript', ['-e', 'tell application "System Events" to key code 12 using {command down, shift down}']);
-}
-exports.screenshot = function() {
-  const spawn_20 = require('child_process').spawn;
-  const ls = spawn_20('osascript', ['-e', 'tell application "System Events" to key code 20 using {command down, shift down}']);
-}
-exports.mute = function() {
-  const spawn_21 = require('child_process').spawn;
-  const ls = spawn_21('osascript', ['-e', 'set volume 0']);
-}
-exports.volumeUp = function() {
-  const spawn_22 = require('child_process').spawn;
-  const ls = spawn_22('osascript', ['-e', 'set volume output volume (output volume of (get volume settings) + 1) --100%']);
-}
+exports.lightsUp = createKey('113');
+exports.lightsDown = createKey('107');
+exports.highlightPrevious = createKeyCmd('126');
+exports.densityDown = createKey('96');
+exports.densityUp = createKey('97');
+exports.launchPad = createKey('131');
+exports.splitScreen = createKey('160');
+exports.spreadScreen = createKey('103');
+exports.closeWindow = createKeyCmd('13');
+exports.closeEverything = createKeyCmd('12');
+exports.newTab = createKeyCmd('17');
+exports.newWindow = createKeyCmd('45');
+exports.splitTabs = createKeyCmd('2');
+exports.search = createKeyCmd('3');
+exports.clear = createKeyCmd('40');
+exports.paste = createKeyCmd('9');
+exports.hide = createKeyCmd('46');
+exports.zoom = createKeyCmd('27');
+exports.config = createKeyCmd('43');
+exports.minimize = createKeyCmd('29');
+exports.shutDown = createKey('12 using {command down, shift down}');
+exports.screenshot = createKey('20 using {command down, shift down}');
+exports.mute = create('set volume 0');
+exports.volumeUp = create('set volume output volume (output volume of (get volume settings) + 1) --100%');
